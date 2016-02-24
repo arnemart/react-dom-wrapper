6988513d5b05886fed3e655345058d821fb40110
Arne Martin Aurlien <am@aurlien.net>
2016-02-24 14:50:18 +0100

    Fix tests

--- test/tests.js
+++ test/tests.js
@@ -5,37 +5,36 @@ var React = require('react');
 buster.testCase('React DOM element generation', {
   'simple DOM element': function() {
     var element = DOM(['span', 'hello']);
-
     buster.assert.equals(element.type, 'span');
-    buster.assert.equals(element._store.props.children, 'hello');
+    buster.assert.equals(element.props.children, 'hello');
   },
 
   'DOM element with class names': function() {
     var element = DOM(['div.test1.test2', 'hello']);
-    buster.assert.equals(element._store.props.className, 'test1 test2');
+    buster.assert.equals(element.props.className, 'test1 test2');
   },
 
   'DOM element with ID': function() {
     var element = DOM(['p#testid', 'hello']);
-    buster.assert.equals(element._store.props.id, 'testid');
+    buster.assert.equals(element.props.id, 'testid');
   },
 
   'DOM element with ID and class names': function() {
     var element = DOM(['div#testid.test1.test2', 'hello']);
-    buster.assert.equals(element._store.props.className, 'test1 test2');
-    buster.assert.equals(element._store.props.id, 'testid');
+    buster.assert.equals(element.props.className, 'test1 test2');
+    buster.assert.equals(element.props.id, 'testid');
   },
 
   'Class names in props and element name': function() {
     var element = DOM(['span#testid.test1.test2', { className: 'test3' }, 'hello']);
-    buster.assert.equals(element._store.props.className, 'test1 test2 test3');
+    buster.assert.equals(element.props.className, 'test1 test2 test3');
   },
 
   'Props do not displace children': function() {
     var element = DOM(['div', { className: 'test-test' }, 'hello', 'hello2']);
-    buster.assert.equals(element._store.props.className, 'test-test');
-    buster.assert.equals(element._store.props.children[0], 'hello');
-    buster.assert.equals(element._store.props.children[1], 'hello2');
+    buster.assert.equals(element.props.className, 'test-test');
+    buster.assert.equals(element.props.children[0], 'hello');
+    buster.assert.equals(element.props.children[1], 'hello2');
   },
 
   'Nested DOM elements': function() {
@@ -44,8 +43,8 @@ buster.testCase('React DOM element generation', {
       ['span', 'hello'],
       ['span', ['strong', 'hello2']]
     ]);
-    buster.assert.equals(element._store.props.children[0].type, 'span');
-    buster.assert.equals(element._store.props.children[1]._store.props.children.type, 'strong');
+    buster.assert.equals(element.props.children[0].type, 'span');
+    buster.assert.equals(element.props.children[1].props.children.type, 'strong');
   },
 
   'Array of DOM elements': function() {
@@ -57,9 +56,9 @@ buster.testCase('React DOM element generation', {
         ['li.third', { key: '3' }, '3']
       ]
     ]);
-    buster.assert.equals(element._store.props.children.length, 3);
-    buster.assert.equals(element._store.props.children[1].type, 'li');
-    buster.assert.equals(element._store.props.children[1].key, '2');
+    buster.assert.equals(element.props.children.length, 3);
+    buster.assert.equals(element.props.children[1].type, 'li');
+    buster.assert.equals(element.props.children[1].key, '2');
   },
 
   'Deeply nested': function() {
@@ -81,8 +80,8 @@ buster.testCase('React DOM element generation', {
       ]
     ]);
 
-    buster.assert.equals(element._store.props.children[0]._store.props.children._store.props.children.length, 2);
-    buster.assert.equals(element._store.props.children[0]._store.props.children._store.props.children[0].type, 'span');
+    buster.assert.equals(element.props.children[0].props.children.props.children.length, 2);
+    buster.assert.equals(element.props.children[0].props.children.props.children[0].type, 'span');
   },
 
   'Custom component': function() {
@@ -95,7 +94,7 @@ buster.testCase('React DOM element generation', {
     var element = DOM([C, { text: 'Hello!' }]);
 
     buster.assert.equals(element.type, C);
-    buster.assert.equals(element._store.props.text, 'Hello!');
+    buster.assert.equals(element.props.text, 'Hello!');
   }
 
 });
